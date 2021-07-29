32a92b0e0dc834bb9539a0a1a95720f2c0b8010c
jandestiny <jandestinyeze@gmail.com>
2021-07-29 17:02:10 +0200

    new base

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 const express = require("express")
-const apiRouter = require("./routes/api")
+const path = require("path");
 const app = express()
 require('dotenv').config()
-var dht_sensor = require("node-dht-sensor");
-const ccs811 = require("ccs811-sensor")
-air_quality_sensor = ccs811.CCS811()
 const port = process.env.PORT || 3000
 
-//airquality: https://www.npmjs.com/package/@chirimen/ccs811
-//airquality2: npm i ccs811-sensor
-
 app.set("view engine", "ejs")
-app.use("/api", apiRouter)
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
-    get_dht_data()
-    res.render("index")
-    get_air_quality_data()
+  res.sendFile('public')
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
+
 function get_dht_data(){
     dht_sensor.read(22, 4, function(err, temperature, humidity) {
         if (!err) {
-          console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
+          console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);s
         }
       });
 }
